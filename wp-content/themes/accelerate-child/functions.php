ee66c13c5e7e751ae003b759d83e33dbd149ac32
BritishPandaChick <sbartley1080@gmail.com>
2022-06-09 01:25:43 -0400

    Remove older styles. Start adding code to functions.php for custom queries

--- wp-content/themes/accelerate-child/functions.php
+++ wp-content/themes/accelerate-child/functions.php
@@ -23,26 +23,12 @@ function create_custom_post_types() {
             'labels' => array(
                 'name' => _( 'Case Studies' ),
             'singular_name' => _( 'Case Study' )
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array( 'slug' => 'case-studies' ),
-    )
-);
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array( 'slug' => 'case-studies' ),
+		)
+	);
 }
-add_action( 'init', 'create_custom_post_types' );
 
-function accelerate_theme_child_widget_init() {
-    
-    register_sidebar( array(
-        'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
-	    'id' => 'sidebar-2',
-	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
-	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</aside>',
-	    'before_title' => '<h3 class="widget-title">',
-	    'after_title' => '</h3>',
-    ) );
-    
-}
-add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
+add_action( 'init', 'create_custom_post_types' );
