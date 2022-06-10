7caa106f387205eba100517eb32a193687635170
BritishPandaChick <sbartley1080@gmail.com>
2022-06-10 01:21:25 -0400

    Update single-case_studies file and front-page file in child theme

--- wp-content/themes/accelerate-child/single-case_studies.php
+++ wp-content/themes/accelerate-child/single-case_studies.php
@@ -22,33 +22,37 @@ get_header(); ?>
         $image_3 = get_field('image_3');
         $size = "full"; ?>
             
-    <article class="case-study">
+    <article class="case-study clearfix">
         <aside class="case-study-sidebar">
             <h2><?php the_title(); ?></h2>
-            <h5><?php echo $services; ?></h5>
-            <h6>Client: <?php echo $clients; ?></h6>
+            <h4><?php echo $services; ?></h4>
+            <h4>Client: <?php echo $clients; ?></h4>
             
             <?php the_content(); ?>
             
-            <p><a href="<?php echo $link; ?>">Site Link</a></p>
+            <p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></p>
         </aside>
         
         <div class="case-study-images">
-            <?php if($image_1) { ?>
-                <?php echo wp_get_attachment_image( $image_1, $size ); ?>;
-            <?php } ?>
+            <?php if($image_1) { 
+                echo wp_get_attachment_image( $image_1, $size ); 
+            } ?>
             
-            <?php if($image_2) { ?>
-                <?php echo wp_get_attachment_image( $image_2, $size ); ?>;
-            <?php } ?>
+            <?php if($image_2) { 
+                echo wp_get_attachment_image( $image_2, $size ); 
+            } ?>
             
-            <?php if($image_3) { ?>
-                <?php echo wp_get_attachment_image( $image_3, $size ); ?>;
-            <?php } ?>
-        </div>
-        
+            <?php if($image_3) {
+                echo wp_get_attachment_image( $image_3, $size ); 
+            } ?>
+        </div>        
     </article>
     <?php endwhile; // end of the loop. ?>
     </div><!-- #content -->
 </div><!-- #primary -->
+
+<nav id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/case-studies/') ?>"&larr; <span>Back to Work</span></a></div>
+</nav>
+
 <?php get_footer(); ?>
