b844dd491acba0812f044459ba5c1e90ba026818
BritishPandaChick <sbartley1080@gmail.com>
2022-06-13 15:20:07 -0400

    Add custom fields plugin. Finish styling single case studies and fixing bugs with code

--- wp-content/themes/accelerate-child/single-case_studies.php
+++ wp-content/themes/accelerate-child/single-case_studies.php
@@ -12,7 +12,6 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-                <h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
 
