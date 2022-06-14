5394bdf9c47358f68e17bf3e865ab8995e2ac126
BritishPandaChick <sbartley1080@gmail.com>
2022-06-14 01:22:55 -0400

    Add code to get content for single case study templates in single-case_studies.php file

--- wp-content/themes/accelerate-child/single-case_studies.php
+++ wp-content/themes/accelerate-child/single-case_studies.php
@@ -10,11 +10,42 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+		<div id="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');
+				$size = "full"; ?>
+								 
+				<article class="case-study clearfix">
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h4><?php echo $services; ?></h4>
+						<h4>Client: <?php echo $client; ?></h4>
+						
+						<?php the_content(); ?>
+						
+						<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></p>
+					</aside>
+									
+					<div class="case-study-images">
+						<?php if($image_1) { 
+							echo wp_get_attachment_image($image_1, $size);
+						} ?>
+							
+						<?php if($image_2) {
+							echo wp_get_attachment_image($image_2, $size);
+						} ?>
+							
+						<?php if($image_3) {
+							echo wp_get_attachment_image($image_3, $size);
+						} ?>
+					</div>
+				</article>
 			<?php endwhile; // end of the loop. ?>
-
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
