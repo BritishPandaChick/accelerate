b220d7172b5d020b2e4edc01f8b4e74db2e3f8f1
BritishPandaChick <sbartley1080@gmail.com>
2022-07-07 00:54:47 -0400

    Fix issues with case study images

--- wp-content/themes/accelerate-child/single-case_studies.php
+++ wp-content/themes/accelerate-child/single-case_studies.php
@@ -18,38 +18,45 @@ get_header(); ?>
 				$image_1 = get_field('image_1');
 				$image_2 = get_field('image_2');
 				$image_3 = get_field('image_3');
+				$size = "full";
 			 ?>
 			
-			<article class="case-study">
-			  <aside class="case-study-sidebar">
-					  <h2><?php the_title(); ?></h2>
-					  <h4><?php echo $services; ?></h4>
-					  <h4><span>Client: <?php echo $client; ?></span></h4>
-					  
-					  <?php the_content(); ?>
-					  
-					  <p class="read-more-link"><strong><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></strong></p>
-			  </aside>
+				<article class="case-study clearfix">
+				  <aside class="case-study-sidebar">
+						  <h2><?php the_title(); ?></h2>
+						  <h4><?php echo $services; ?></h4>
+						  <h4><span>Client: <?php echo $client; ?></span></h4>
+						  
+						  <?php the_content(); ?>
+						  
+						  <p class="read-more-link"><strong><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></strong></p>
+				  </aside>
 
-			  <div class="case-study-images">
-				<?php if ($image_1) { ?>
-					<img src="<?php echo $image_1; ?>" />
-				<?php } ?>
-				
-				<?php if ($image_2) { ?>
-					<img src="<?php echo $image_2; ?>" />
-				<?php } ?>
-				
-				<?php if ($image_3) { ?>
-					<img src="<?php echo $image_3; ?>" />
-				<?php } ?>
-			  </div>
-		</article>
-				<?php the_content(); ?>
+				  <div class="case-study-images">
+					<?php if ($image_1) { 
+						echo wp_get_attachment_image( $image_1, $size );
+					} ?>
+					
+					<?php if ($image_2) { 
+						echo wp_get_attachment_image( $image_2, $size );
+					} ?>
+					
+					<?php if ($image_3) {
+						echo wp_get_attachment_image( $image_3, $size );
+					} ?>
+				  </div>
+				</article>
 				
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
+	
+	<!-- Back to Work Navigation -->
+	<nav id="navigation" class="container">
+		<div class="left">
+			<a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to Work</span></a></div>
+		</div>
+	</nav>
 
 <?php get_footer(); ?>
