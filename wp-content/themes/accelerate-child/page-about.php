7c4c6775ccf6e25ccc7163a83ca27ee792787b07
BritishPandaChick <sbartley1080@gmail.com>
2022-08-16 01:24:18 -0400

    Change fields for about page

--- wp-content/themes/accelerate-child/page-about.php
+++ wp-content/themes/accelerate-child/page-about.php
@@ -1,5 +1,6 @@
 <?php 
-/*
+/**
+*Template Name: About page 
 *Template for displaying the About page
 *Description: template for displaying the About page
 *
@@ -11,86 +12,54 @@
 
 get_header(); ?>
 
-<section class="about-page">
-    <div class='homepage-hero'>
-        <?php the_content(); ?>
-        <h2><span class="about-co"><span style="color:#45ac9d";>Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</span></h2>
-    </div>
-    
-    <div class="site-content">
-        
-        <?php while ( have_posts() ) : the_post();
-        
-            $description_1 = get_field('description_1');
-            $description_2 = get_field('description_2');
-            $description_3 = get_field('description_3');
-            $description_4 = get_field('description_4');
-            $image_1 = get_field('image_1');
-            $image_2 = get_field('image_2');
-            $image_3 = get_field('image_3');
-            $image_4 = get_field('image_4');
-            $size = "full"; ?>
-        
-        <div class="about-services">
-            <h3>Our Services</h3>
-            <br/>
-            <p>We take pride in our clients and the content we create for them.</p>
-            <p>Here is a brief overview of our offered services.</p>
-        </div>
-    
-        <div class="what-we-do">
-            <div class="about-image-1">
-                <?php if($image_1) { ?>
-                <?php echo wp_get_attachment_image( $image_1, $size ); ?>
-                <?php } ?>
-            </div>
-            
-            <div class="content-strategy">
-                <h2>Content Strategy</h2>
-                <?php echo $description_1; ?>
-            </div>
-            
-            <div class="influencer-mapping">
-                <h2>Influencer Mapping</h2>
-                <?php echo $description_2; ?>
-            </div>
-            
-            <div class="about-image-2">
-                <?php if($image_2) { ?>
-                <?php echo wp_get_attachment_image( $image_2, $size ); ?>
-                <?php } ?>
-            </div>
-            
-            <div class="about-image-3">
-                <?php if($image_3) { ?>
-                <?php echo wp_get_attachment_image( $image_3, $size ); ?>
-                <?php } ?>
-            </div>
-            
-            <div class="social-media-strategy">
-                <h2>Social Media Strategy</h2>
-                <?php echo $description_3; ?>
-            </div>
-            
-            <div class="design-development">
-                <h2>Design & Development</h2>
-                <?php echo $description_4; ?>
-            </div>
-            
-            <div class="about-image-4">
-                <?php if($image_4) { ?>
-				<?php echo wp_get_attachment_image( $image_4, $size ); ?>
-				<?php } ?>
-            </div>
-        </div>
-    
-    <div class="work-with-us">
-        <h2 class="interested">Interested in working with us?</h2>
-        <a class="contact-button" href="<?php echo home_url(); ?>/contact-us">Contact Us</a>
-    </div> 
-    <?php endwhile; // end of the loop. ?>
-        
-    </div><!-- .container -->
-</section><!-- .about-page -->
+<header class="home-page about-adjust">
+	<h3 class="about-header-text">
+Accelerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.
+</h3>
+</header>
+
+<div id="primary" class="site-content ">
+	<div class="main-content" role="main">
+		<div class="center-text">
+			<h2>Our Services</h2>
+			<p>We take pride in our clients and the content we create for them.
+			<br>Here’s a brief overview of our offered services.</p>
+		</div>
+
+
+	<?php query_posts('post_type=services'); ?>
+
+		<?php while ( have_posts() ) : the_post();
+			$service = get_field('service');
+			$service_descr = get_field('service_descr');
+			$image_1 = get_field('image_icon');
+			$size = "medium";
+		?>
+		<div class="flex-wrapper">
+			<div class="service-loop-wrap">
+
+				<div class="service-text">
+					<h2><?php the_title(); ?></h2>
+					<p><?php echo $service_descr; ?></p>
+				</div>
+
+				<div class="service-img">
+					<img src=<?php if($image_1) {
+						echo wp_get_attachment_image( $image_1, $size );
+					}?>
+				</div>
+				
+				<div class="clearfix-about"></div>
+			</div>
+		</div>
+
+		<?php endwhile; // end of the loop. ?>
+		
+		<div class="work-with-us">
+			<h2>Interested in working with us?</h2>
+			<a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+		</div>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
