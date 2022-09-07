e591cc92e371fd3a07fc3eb3fb67f6954d232a17
BritishPandaChick <sbartley1080@gmail.com>
2022-09-06 22:51:09 -0400

    Add styles for 404 page. Start playing around with about page styles

--- wp-content/themes/accelerate-child/page-about.php
+++ wp-content/themes/accelerate-child/page-about.php
@@ -15,7 +15,7 @@ get_header(); ?>
     <div class='homepage-hero'>
         <?php the_content(); ?>
         <h2>
-			<span class="about-co"><span style="color:#45ac9d";>Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.
+			<span class="about-co">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.
 			</span>
 		</h2>
     </div>
